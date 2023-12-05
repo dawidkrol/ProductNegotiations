50976f76e63157d571f220fee171ad067ce7933f
Dawid Król <dawidkrol7@gmail.com>
2023-12-05 18:43:12 +0100

    Implementing services

---
+++ ProductNegotiations/Models/NegotiationClientModel.cs
@@ -0,0 +1,13 @@
+﻿using ProductNegotiations.Database.Library.Models;
+
+namespace ProductNegotiations.API.Models
+{
+    public class NegotiationClientModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public ProductDbModel Product { get; set; }
+        public string? AdditiionalInformations { get; set; }
+        public decimal ProposedPrice { get; set; }
+    }
+}
